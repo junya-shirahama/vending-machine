4a5ad5e3a2b9a112ade6198d3143dc393ce845b8
Junya Shirahama <>
2018-07-28 19:32:11 +0900

    DB接続ができた

---
+++ src/main/java/com/practice/Drink.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author JunyaShirahama
+ */
+@Entity
+@Table(name = "drink")
+@Getter
+@Setter
+public class Drink {
+
+    @Id
+    private Integer drinkId;
+
+    private String drinkName;
+
+    private Integer price;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into drink(DRINK_ID, drink_name, price) values (1, 'orange juice',  120);
